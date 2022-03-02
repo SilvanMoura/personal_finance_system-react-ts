7806328ea8185067f4429ec05b41ca3b2e12cacc
Silvan Moura <89323396+SilvanMoura@users.noreply.github.com>
2022-03-02 13:20:17 -0300

    criando filtro por mês e começando componente Table

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import * as C from './App.styles';
 import {Item} from './types/Item';
 import {Category} from './types/Category';
 import {categories} from './data/categories';
 import {items} from './data/items';
+import {getCurrentMonth, filterListByMonth} from './helpers/dataFilters'
 
 let App = ()=>{
   let [list, setList] = useState(items);
-  let [currentMonth, setCurrentMonth] = useState();
-  
+  let [filteredList, setFilteredList] = useState<Item[]>([])
+  let [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(()=>{
+    setFilteredList( filterListByMonth( list, currentMonth) )
+  }, [list, currentMonth])
+
   return(
     <C.Container>
       
---
+++ src/components/TableArea/styles.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export let Table = styled.table`
+
+`;
+
+export let TableHeadColumn = styled.th`
+    
+`;
